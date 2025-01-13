d63730bf1ca8683680f6f35f86782bfe53fed56d
Ayush Poddar <115064784+Ayush-Poddar-2003@users.noreply.github.com>
2025-01-13 08:17:23 +0000

    Till 1:14:37

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "69"
+console.log(typeof score)
+
+let valueInInteger = Number(score)
+console.log(typeof valueInInteger)
+
+let name = "Ayush69"
+let ModifiedName = Number(name)
+console.log(typeof ModifiedName)
+console.log(ModifiedName)  //Will show Nan which is also a datatype
+
+let a = 0
+let a2 = Number(a)
+console.log(typeof a)
+console.log(a)  //Will show 0
+
+/**Basic conversions
+ * "33" => 33
+ * "3a" => NaN
+ * true => 1
+ * false => 0
+ * 
+ * BOOL CONVERSIONS
+ * 0 => false
+ * 1 => true
+ * "" => false
+ * "a" => true
+ */
